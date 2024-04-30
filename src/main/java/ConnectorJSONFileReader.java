8c43a42834f4a1812484779a39404ca8d236f5c9
Mathewos Yohannes <myyohannes@gmail.com>
2024-04-30 12:46:32 -0700

    Adding JSONFileReader -Inductor, updated ConnectorsJSONFileReader

--- src/main/java/ConnectorJSONFileReader.java
+++ src/main/java/ConnectorJSONFileReader.java
@@ -35,8 +35,8 @@ public class ConnectorJSONFileReader {
 
         String categoryFolder = "Connectors, Interconnects";
 
-//        String directory = "/root/EDAProject/Postman Exports/";
-        String directory = "Postman Exports/";
+        String directory = "/root/EDAProject/Postman Exports/";
+//        String directory = "Postman Exports/";
 
         List<String> filesInDirectory = DirectoryFiler.getFileNamesInDirectory(directory + categoryFolder);
 
